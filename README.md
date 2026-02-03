cc7163d20e9253358a3cbc9b02bb8b28c3b66dc3
Utkarsh Sharma <90753570+usa1204119@users.noreply.github.com>
2026-02-04 00:22:27 +0530

    Sync from collaborative IDE

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # voice-wrapper-assesment
-Recording Assesment
+
+This project was created with React.
+
+## Getting Started
+
+```bash
+npm install
+npm start
+```
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to React</h1>
+        <p>Start editing to see some magic happen!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
